852b03906897895d1eea27e48c931a0ad9da5ab3
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 21:42:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,24 +2,6 @@
 
 def position_taken(arr, int)
   if arr[0] == int
-    true
-  elsif arr[1] == int
-    true
-  elsif arr[2] == int
-    true
-  elsif arr[3] == int
-    true
-  elsif arr[4] == int
-    true
-  elsif arr[5] == int
-    true
-  elsif arr[6] == int
-    true
-  elsif arr[7] == int
-    true
-  elsif arr[8] == int
-    true
-  else
-    false
+
   end
 end
